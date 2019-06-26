39fe7c19e6f24c20f5cfc90742f0a97a874bcc98
user <gibson.stephen92@gmail.com>
2019-06-26 11:14:49 +0100

    working on card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Button from "./components/Button/Button";
 import InputDefault from "./components/Input/InputDefault";
 import InputLabel from "./components/Input/InputLabel";
 import InputSelect from "./components/Input/InputSelect";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
         name="test-select"
         options={["Option A", "Option B"]}
       />
+      <Card>Look at me!</Card>
     </div>
   );
 }
---
+++ src/components/MeetingCard/MeetingCard.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from "react";
+
+export type Props = {
+  children?: any
+};
+
+const MeetingCard = (props: Props) => {
+  return (
+    <div className="card">
+      <span>ID</span>
+    </div>
+  );
+};
+
+export default MeetingCard;
